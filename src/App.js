1171f2d2ac6b2600f7bcee0b7868c31845f14f4e
kelson-cosme <kelson.almeida123@gmail.com>
2023-06-05 21:17:14 -0400

    Customizar as cores

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 // import style from "./bootstrap-5.3.0-dist/css/bootstrap.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./custom.css"
 
 function App() {
   return (
@@ -20,10 +21,5 @@ function App() {
   );
 }
 
-{/* <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-  crossorigin="anonymous"
-/> */}
+
 export default App;
