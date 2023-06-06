fbe9cba55ce51db1c9aa3234af6f26c02b7bd4f3
kelson-cosme <kelson.almeida123@gmail.com>
2023-06-05 20:57:36 -0400

    Cores bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
-import Badge from 'react-bootstrap/Badge';
-
+// import style from "./bootstrap-5.3.0-dist/css/bootstrap.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-      <Badge bg="secondary" as={Button}>
-          New
-        </Badge>
-      </h1>
+      <h1 className='text-primary'>Ola mundo</h1>
+      <h1 className='text-secondary'>Ola mundo</h1>
+      <h1 className='text-success'>Ola mundo</h1>
+      <h1 className='text-danger'>Ola mundo</h1>
+      <h1 className='text-warnig'>Ola mundo</h1>
+      <h1 className='text-info'>Ola mundo</h1>
+      <h1 className='text-light bg-black'>Ola mundo</h1>
+      <h1 className='text-dark'>Ola mundo</h1>
+      <h1 className='text-body'>Ola mundo</h1>
+      <h1 className='text-muted '>Ola mundo</h1>
+
     </div>
   );
 }
 
+{/* <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+  crossorigin="anonymous"
+/> */}
 export default App;
