5e49463ec007fd55f8fd33577551c0d6795a0337
kelson.almeida <kelson.almeida123@gmail.com>
2023-07-07 14:13:35 -0400

    Container, Break point

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import "./custom.css"
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-primary'>Ola mundo</h1>
-      <h1 className='text-secondary'>Ola mundo</h1>
-      <h1 className='text-success'>Ola mundo</h1>
-      <h1 className='text-danger'>Ola mundo</h1>
-      <h1 className='text-warnig'>Ola mundo</h1>
-      <h1 className='text-info bg-success'>Ola mundo</h1>
-      <h1 className='text-light bg-black'>Ola mundo</h1>
-      <h1 className='text-dark'>Ola mundo</h1>
-      <h1 className='text-body'>Ola mundo</h1>
-      <h1 className='text-muted '>Ola mundo</h1>
+    <div className="App p-5 bg-dark">
+      <div className="container ">
+        <h1 className='text-primary'>Ola mundo</h1>
+        <h1 className='text-secondary'>Ola mundo</h1>
+        <h1 className='text-success'>Ola mundo</h1>
+        <h1 className='text-danger'>Ola mundo</h1>cdc
+        <h1 className='text-warnig'>Ola mundo</h1>
+        <h1 className='text-info bg-success'>Ola mundo</h1>
+        <h1 className='text-light bg-black'>Ola mundo</h1>
+        <h1 className='text-dark'>Ola mundo</h1>
+        <h1 className='text-body'>Ola mundo</h1>
+        <h1 className='text-muted '>Ola mundo</h1>
+      </div>
+      
 
 
       <button type='button' className='btn btn-danger'>Cancelar</button>
